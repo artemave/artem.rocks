5f2e3e687d355b530c1c3e1ee6c0b8926cc43bba
artemave <artemave@gmail.com>
2023-10-13 10:35:36 +0200

    Better wording

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,10 +17,10 @@ export default function Index() {
         <Intro />
         <Content>
           <H1>About</H1>
-          <p>Born in Russia, I moved to London at the age of thirty, where I spent most of my career as a web developer. Around 2009, I switched from Perl to Ruby on Rails and picked up Node a few years later. Ever since I've been going back and forth between the two. I also took a lot of interest in automated testing (even wrote a <Link href="https://github.com/artemave/assert-raisins" target='_blank'>test runner</Link>!) and I consider this to be my strong side.</p>
+          <p>I've been doing web development since I moved to London in 2007. It was Perl to begin with, then Ruby on Rails, and then Node. I settled on the latter two and ever since been switching back and forth. I also took a lot of interest in automated testing (even wrote a <Link href="https://github.com/artemave/assert-raisins" target='_blank'>test runner</Link>!) and I consider this to be my strong side.</p>
           <p>Besides work, I've also dipped my toes into Go, Python, Dart, Lua, and Vimscript.</p>
 
-          <p>I am currently living in France (I am a French citizen). I speak native Russian, fluent English, and reasonable French.</p>
+          <p>I live in France at the moment (I am a French citizen). I speak fluent English, and reasonable French.</p>
           <ButtonGroup className='mt-10'>
             <ButtonLink href='https://cv.artem.rocks'>View my CV</ButtonLink>
           </ButtonGroup>
