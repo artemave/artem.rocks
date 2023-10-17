0d881c59b5b31d9f5fe75bb77567235aa59ec7ec
artemave <artemave@gmail.com>
2023-10-17 11:21:36 +0200

    Change theme color

--- components/meta.tsx
+++ components/meta.tsx
@@ -4,8 +4,8 @@ const Meta = () => {
   return (
     <Head>
       <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="theme-color" content="#000" />
+      <meta name="msapplication-TileColor" content="rgb(30 41 59)" />
+      <meta name="theme-color" content="rgb(30 41 59)" />
       <meta name="description" content="Personal website and tech blog." />
     </Head>
   )
