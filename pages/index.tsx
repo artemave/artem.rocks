67d22272b634d00f24b7055c8d4d855553eaae37
artemave <artemave@gmail.com>
2023-11-10 17:56:23 +0100

    Update about text

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Layout from '../components/layout'
 import Head from 'next/head'
 import Content from '../components/content'
 import H1 from '../components/H1'
-import Link from '../components/Link'
 import ButtonLink from '../components/ButtonLink'
 import ButtonGroup from '../components/ButtonGroup'
 
@@ -17,10 +16,8 @@ export default function Index() {
         <Intro />
         <Content>
           <H1>About</H1>
-          <p>I've been doing web development since I moved to London in 2007. It was Perl to begin with, then Ruby on Rails, and then Node. I settled on the latter two and ever since been switching back and forth. I also took a lot of interest in automated testing (even wrote a <Link href="https://github.com/artemave/assert-raisins" target='_blank'>test runner</Link>!) and I consider this to be my strong side.</p>
-          <p>Besides work, I've also dipped my toes into Go, Python, Dart, Lua, and Vimscript.</p>
-
-          <p>I live in France at the moment (I am a French citizen). I speak fluent English, and reasonable French.</p>
+          <p>I am a seasoned full-stack web developer with over fifteen years of experience. Throughout my career, I've mainly gone back and forth between Rails and Node, with occasional forays into SPAs. I've worked with a variety of clients - from small startups to enterprises.</p>
+          <p>I like to understand the business before coding. I like to translate this understanding into automated scenarios and tests. I prefer clear over clever. I like to deploy fast, small, and often. I like to know about problems in production before the users. I like to talk to the users. I like to talk to the business. I like to talk to the colleagues and pair programming. And, above all, I like coding - it's not just a job, it's a passion.</p>
           <ButtonGroup className='mt-10'>
             <ButtonLink href='https://cv.artem.rocks'>View my CV</ButtonLink>
           </ButtonGroup>
