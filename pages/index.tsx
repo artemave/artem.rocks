ae8114f4c28a057fd53fccd1d8d64aa8a953d482
artemave <artemave@gmail.com>
2024-02-07 21:49:08 +0100

    New blog post

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Index() {
           <H1>About</H1>
           <p>I am a seasoned full-stack web developer with over fifteen years of experience. Throughout my career, I've mainly gone back and forth between Rails and Node, with occasional forays into SPAs. I've worked with a variety of clients - from small startups to enterprises.</p>
           <p>I like to understand the business before coding. I like to translate this understanding into automated scenarios and tests. I prefer clear over clever. I like to deploy fast, small, and often. I like to know about problems in production before the users. I like to talk to the users. I like to talk to the business. I like to talk to the colleagues and pair programming. And, above all, I like coding - it's not just a job, it's a passion.</p>
-          <ButtonGroup className='mt-10'>
+          <ButtonGroup className='mt-12'>
             <ButtonLink href='https://cv.artem.rocks'>View my CV</ButtonLink>
           </ButtonGroup>
         </Content>
